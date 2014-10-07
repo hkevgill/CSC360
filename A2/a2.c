25bca7dedb2f59e9413a71c1ec6c438c8d0d98d2
Hardeep Gill <hkevgill@n-iota.csc.uvic.ca>
2014-10-06 18:26:12 -0700

    Waiting now works for each train (thread)

--- A2/a2.c
+++ A2/a2.c
@@ -4,10 +4,18 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <assert.h>
+#include <errno.h>
 #define MAXLINE 100
 
 /* Global Variables */
 FILE *fp;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct train{
+	int loadTime;
+	int crossTime;
+	char priority[1];
+}train;
 
 /*
  * Removes newline '\n' characters
@@ -40,14 +48,23 @@ void parse(char ReadLine[MAXLINE], char *tokens[1024], char *cmd_in){
 
 }
 
+void *trains(void *args){
+
+	train *t_cpy = args;
+	sleep(t_cpy->loadTime);
+
+	printf("THREAD FINISHED %d\n", t_cpy->loadTime);
+
+	return ((void *)0);
+}
+
 int main(int argc, char *argv[]){
 
 	char ReadLine[MAXLINE];
 	char *tokens[1024];
 	char *cmd_in = (char*) malloc(sizeof(char) * 100);
 	int count;
-	int loadTime;
-	int crossTime;
+	int ret;
 
 	if(argc != 3){
 		printf("usage: %s filename\n", argv[0]);
@@ -60,18 +77,34 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 
+	pthread_t *thread = (pthread_t *)malloc(sizeof(pthread_t)*atoi(argv[2]));
+
 	for(count = 0; count < atoi(argv[2]); count++){
 		fgets(ReadLine, MAXLINE, fp);
 		chomp(ReadLine);
 		parse(ReadLine, tokens, cmd_in);
 
-		printf("%s ", tokens[0]);
-		printf("%s ", tokens[1]);
-		printf("%s\n", tokens[2]);
+		train *t = (train*)malloc(sizeof(train));
+		t->priority[0] = atoi(tokens[0]);
+		t->loadTime = atoi(tokens[1]);
+		t->crossTime = atoi(tokens[2]);
+
+
+		ret = pthread_create(&thread[count], NULL, &trains, t);
+		if(ret != 0){
+			perror("pthread create error\n");
+		}
+
 	}
 
+	int i;
+	for(i = 0; i < atoi(argv[2]); i++){
+		pthread_join(thread[i], NULL);
+	}
 
+	printf("MAIN FINISHED!!!\n");
 
+	free(cmd_in);
 	fclose(fp);
 	return 0;
 }
